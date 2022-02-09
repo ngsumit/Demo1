db081f2561d4458922b585f003d878d6eeca5f7a
sumit <sumit@LAPTOP-C6RVQEFQ>
2022-02-09 18:00:28 +0530

    First Commit

---
+++ Sumit/src/Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 6;
+		int sum;
+		sum = a+b;
+		
+System.out.println("Sum of these two number: "+ sum);
+	}
+
+}
