a62129c27baa8da51d929f903249e61a5e869637
sumit <sumit@LAPTOP-C6RVQEFQ>
2022-02-11 14:14:11 +0530

    Commit

---
+++ Sumit/src/day4/Addition1.java
@@ -0,0 +1,13 @@
+package day4;
+import java.util.*;
+import java.util.Scanner;
+public class Addition1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+System.out.println("Enter your name");
+String name = in.next();
+in.close();
+	}
+
+}
