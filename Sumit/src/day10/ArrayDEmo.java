d14328ac0b26009d1bbc702519b95f0c8c8682c9
sumit <sumit@LAPTOP-C6RVQEFQ>
2022-02-18 16:55:00 +0530

    Commit

--- Sumit/src/day10/ArrayDEmo.java
+++ Sumit/src/day10/ArrayDEmo.java
@@ -1,10 +1,14 @@
 package day10;
 
 public class ArrayDEmo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+public static void main(String args[]){  
+		int a[]=new int[5]; 
+		a[0]=10;  
+		a[1]=20;  
+		a[2]=70;  
+		a[3]=40;  
+		a[4]=50;  
+		  
+		for(int i=0;i<a.length;i++)  
+		System.out.println(a[i]);  
+		}}  
