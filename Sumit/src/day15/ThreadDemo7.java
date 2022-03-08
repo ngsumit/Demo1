b69c94e2c0c556542a4b1e4d900e0f0072aaa0a0
sumit <sumit@LAPTOP-C6RVQEFQ>
2022-03-08 16:43:32 +0530

    Commit

---
+++ Sumit/src/day15/ThreadDemo7.java
@@ -0,0 +1,22 @@
+package day15;
+
+public class ThreadDemo7 extends Thread {
+	
+	public void run() {
+		int n = 0;
+		while(n < 5000) {
+			System.out.println("Count below 5000");
+		}
+		else
+			System.out.println("Count ");
+			}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadDemo7 td = new ThreadDemo7();
+		Thread th1 = new Thread();
+		Thread th2 = new Thread();
+		th1.start();
+	}
+
+}
